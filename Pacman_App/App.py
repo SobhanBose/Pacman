3e69c2da64b6d5ed0c23b9310f8290c649a92e77
SobhanBose <bosesobhan2003@gmail.com>
2021-09-10 11:04:27 +0530

    Added maze backgroud and grid system

--- Pacman_App/App.py
+++ Pacman_App/App.py
@@ -9,40 +9,43 @@ vec = pygame.math.Vector2()
 
 class App:
     def __init__(self) -> None:
-        self.__width = WIDTH
-        self.__height = HEIGHT
+        self.__width = SCREEN_WIDTH
+        self.__height = SCREEN_HEIGHT
+        self.__mazeWidth = MAZE_WIDTH
+        self.__mazeHeight = MAZE_HEIGHT
         self.__fps = FPS
         self.__running = True
+        self.__currentscore = 0
+        self.__highscore = 0
         self.__screen = pygame.display.set_mode((self.__width, self.__height))
         self.__clock = pygame.time.Clock()
         self.__gamestate = "intro_screen"
+        self.__cellWidth = self.__mazeWidth//28
+        self.__cellHeight = self.__mazeHeight//30
+        self.load()
+    
     
     def getWidth(self) -> int:
         return self.__width
     
+
     def getHeight(self) -> int:
         return self.__height
     
+
     def getGameState(self) -> str:
         return self.__gamestate
     
+
     def setGameState(self) -> str:
         return self.__gamestate
 
-    def run(self) -> None:
-        while self.__running:
-            if self.__gamestate == "intro_screen":
-                self.intro_events()
-                self.intro_update()
-                self.intro_draw()
-            elif self.__gamestate == "playing":
-                self.playing_events()
-                self.playing_update()
-                self.playing_draw()
-            self.__clock.tick(self.__fps)
-        pygame.quit()
-        sys.exit(0)
-    
+
+    def load(self) -> None:
+        self.__background = pygame.image.load(r'Pacman_App\res\images\maze.png')
+        self.__background = pygame.transform.scale(self.__background, (self.__mazeWidth, self.__mazeHeight))
+
+
     def draw_text(self, msg: str, screen: pygame.display, align: str, size: int, color: tuple, font_face: str) -> None:
         """
             align ([string]): [space separated strings - center_horizontal, right, left center_vertucal, top, bottom or absolute positions in format 'horizontal_position <space> vertical_position']
@@ -69,6 +72,28 @@ class App:
         elif align[1] == 'bottom':
             pos[1] = self.__height-text.get_height()-10
         screen.blit(text, pos)
+    
+
+    def draw_grid(self) -> None:
+        for cols in range(self.__mazeWidth//self.__cellWidth):
+            pygame.draw.line(self.__screen, GRAY, (cols*self.__cellWidth+SCREEN_SIZE_BUFFER, SCREEN_SIZE_BUFFER), (cols*self.__cellWidth+SCREEN_SIZE_BUFFER, self.__mazeHeight+SCREEN_SIZE_BUFFER))
+        for rows in range(self.__mazeHeight//self.__cellHeight):
+            pygame.draw.line(self.__screen, GRAY, (SCREEN_SIZE_BUFFER, rows*self.__cellHeight+SCREEN_SIZE_BUFFER), (self.__mazeWidth+SCREEN_SIZE_BUFFER, rows*self.__cellHeight+SCREEN_SIZE_BUFFER))
+
+
+    def run(self) -> None:
+        while self.__running:
+            if self.__gamestate == "intro_screen":
+                self.intro_events()
+                self.intro_update()
+                self.intro_draw()
+            elif self.__gamestate == "playing":
+                self.playing_events()
+                self.playing_update()
+                self.playing_draw()
+            self.__clock.tick(self.__fps)
+        pygame.quit()
+        sys.exit(0)
 
 
     def intro_events(self) -> None:
@@ -79,8 +104,10 @@ class App:
                 if event.key == pygame.K_SPACE:
                     self.__gamestate = "playing"
     
+
     def intro_update(self) -> None:
         pass
+
 
     def intro_draw(self) -> None:
         self.__screen.fill(BLACK)
@@ -90,13 +117,21 @@ class App:
         self.draw_text("HIGH SCORE: ", self.__screen, f'center_horizontal bottom' ,FONT_SIZE_INTRO, FONT_COLOR_WHITE, FONT_FACE_INTRO)
         pygame.display.update()
     
+
     def playing_events(self) -> None:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.__running = False
     
+
     def playing_update(self) -> None:
         pass
 
+
     def playing_draw(self) -> None:
-        pass
+        self.__screen.fill(BLACK)
+        self.__screen.blit(self.__background, (SCREEN_SIZE_BUFFER, SCREEN_SIZE_BUFFER))
+        self.draw_grid()
+        self.draw_text(f"HIGH SCORE: {self.__highscore}", self.__screen, f'left top' , 16, FONT_COLOR_WHITE, FONT_FACE_INTRO)
+        self.draw_text(f"CURRENT SCORE: {self.__currentscore}", self.__screen, f'right top' , 16, FONT_COLOR_WHITE, FONT_FACE_INTRO)
+        pygame.display.update()
